1f24647cf4defa9ccdf88bbdb4843028b7bcf426
Arif Yüksel <arifyuksel-fb@hotmail.com>
2021-12-11 22:26:40 +0300

    json files

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__, static_url_path='')
 def home():
    return render_template('index.html')
 
+
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory('static', path)
